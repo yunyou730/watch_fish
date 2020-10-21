bf9ce45126e9bff3f74bf34a3d9b3c7ccf5dbffc
ayy <502765299@qq.com>
2020-10-21 23:40:12 +0800

    +增加  Lua 调用  C# 的测试

---
+++ watch_fish_client/Assets/_scripts/TestMono.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace  watchfish
+{
+    public class TestMono : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("This is TestMono,name:" + gameObject.name);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Debug.Log("earn money");
+        }
+    
+        public void TestFunc(string str)
+        {
+            Debug.Log("This is test func,str " + str);
+            gameObject.name = gameObject.name + " | " + str;
+        }
+    
+        public static TestMono Add(GameObject go)
+        {
+            return go.AddComponent<TestMono>();
+        }
+    }
+    
+}
