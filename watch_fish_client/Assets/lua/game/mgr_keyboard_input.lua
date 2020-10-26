88eb5fe149cfac63e65deaea5f57cba9f86a8bc5
ayy <502765299@qq.com>
2020-10-27 00:42:58 +0800

    +修改  Lua  监听键盘输入 错误的问题

--- watch_fish_client/Assets/lua/game/mgr_keyboard_input.lua
+++ watch_fish_client/Assets/lua/game/mgr_keyboard_input.lua
@@ -1,26 +1,26 @@
 ---@class CMgrKeyboardInput
 CMgrKeyboardInput = Core.declare_class()
---[[
+
 local _care_keys_map = {
-    [CS.UnityENgine.KeyCode.F1] = function()
+    [CS.UnityEngine.KeyCode.F1] = function()
         print("this is f1")
     end,
 
-    [CS.UnityENgine.KeyCode.F2] = function()
+    [CS.UnityEngine.KeyCode.F2] = function()
         print("this is f2")
     end,
     
-    [CS.UnityENgine.KeyCode.F3] = function()
+    [CS.UnityEngine.KeyCode.F3] = function()
         print("this is f3")
     end,    
 }
-]]
+
 function CMgrKeyboardInput:OnUpdate()
-    -- if CS.UnityEngine.Input.anyKey then
-    --     for key_code,func in pairs(_care_keys_map) do
-    --         if CS.UnityEngine.Input.GetKeyDown(key_code) then
-    --            func()
-    --         end
-    --     end
-    -- end
+    if CS.UnityEngine.Input.anyKey then
+        for key_code,func in pairs(_care_keys_map) do
+            if CS.UnityEngine.Input.GetKeyDown(key_code) then
+               func()
+            end
+        end
+    end
 end
--- watch_fish_client/Assets/lua/game/view/menu_login.lua
+++ watch_fish_client/Assets/lua/game/view/menu_login.lua
@@ -2,7 +2,12 @@
 CMenuLogin = Core.declare_class(CMenuBase)
 
 function CMenuLogin:ctor()
-    self._prefab_path = "menu/menu_login.prefab"
+    self._prefab_path           = "menu/menu_login.prefab"
+    self._btn_login             = nil
+    self._text_username         = nil
+    self._text_password         = nil
+    self._input_username        = nil
+    self._input_password        = nil
 end
 
 function CMenuLogin:dtor()
@@ -11,4 +16,5 @@ end
 
 function CMenuLogin:EnterWithArg(enter_arg)
     self.__super.EnterWithArg(self,enter_arg)
+
 end
